28d5533ec7e12945a38a1c5a160749f590ff2ffe
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-03-18 20:30:43 +0630

    feat: add error component

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router'
+
+import error from '../../assets/error.png'
+import styles from './Error.module.scss'
+
+interface ErrorProps {
+  errorMsg?: string
+  refetchFunc?: () => void
+}
+
+const Error = (props: ErrorProps) => {
+  const navigate = useNavigate()
+
+  const refreshPage = () => navigate(0)
+
+  const { errorMsg = 'Something went wrong!', refetchFunc = refreshPage } = props
+
+  return (
+    <div className={styles.error}>
+      <img src={error} alt='Error' />
+
+      <div>
+        <h2>{errorMsg}</h2>
+
+        <p>
+          Click <span onClick={refetchFunc}>here</span> to try again.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Error
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Post from './Post/Post'
 import Comments from './Comments/Comments'
 import CommentItem from './CommentItem/CommentItem'
 import LoadingSpinner from './LoadingSpinner/LoadingSpinner'
+import Error from './Error/Error'
 
-export { Post, Comments, CommentItem, LoadingSpinner }
+export { Post, Comments, CommentItem, LoadingSpinner, Error }
