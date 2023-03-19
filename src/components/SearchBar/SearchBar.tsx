9aadeb0f9869dc8a8310d1614cb8126e6dbe6ffd
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-03-19 15:34:30 +0630

    feat: implement post searching

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,20 @@
 import { useDeferredValue, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 
+import { setSearchKeyword } from '../../store/posts/postsSlice'
 import styles from './SearchBar.module.scss'
 
 const SearchBar = () => {
+  const dispatch = useDispatch()
+
   const [keyword, setKeyword] = useState<string>('')
 
   const deferredKeyword = useDeferredValue(keyword)
 
-  useEffect(() => {}, [deferredKeyword])
+  // This effect will ran immediately as no intensive calculation is needed to make the update
+  useEffect(() => {
+    dispatch(setSearchKeyword(deferredKeyword))
+  }, [deferredKeyword, dispatch])
 
   return (
     <input
--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,22 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { Error, LoadingSpinner, Post, SearchBar } from '../../components'
+import { useAppSelector } from '../../hooks'
 import { getPosts } from '../../api'
 import styles from './Posts.module.scss'
 
 const Posts = () => {
+  const searchKeyword = useAppSelector((state) => state.posts.searchKeyword)
+
   const { data, isLoading, isError } = useQuery({ queryKey: ['posts'], queryFn: getPosts })
 
   if (isLoading) return <LoadingSpinner />
 
   if (isError) return <Error />
 
+  const matchedPosts = data.data.filter((post) => post.title.includes(searchKeyword))
+
   return (
     <div className={styles.container}>
       <SearchBar />
 
       <div className={styles['posts-container']}>
-        {data.data.map((post) => (
+        {matchedPosts.length === 0 && <p>Posts not found!</p>}
+
+        {matchedPosts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
